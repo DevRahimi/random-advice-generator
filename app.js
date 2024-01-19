f9a745298117ec854ead7f12e31e21a4a2ff7600
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2024-01-19 11:46:21 +0200

    Built async fetch function

---
+++ app.js
@@ -0,0 +1,18 @@
+const card = document.querySelector('.card');
+const btn = card.querySelector('.card__btn');
+const adviceId = card.querySelector('.card__advice-id');
+const adviceDisplay = card.querySelector('.card__advice-text');
+
+const fetchWisdom = async () => {
+	try {
+		const res = await fetch('https://api.adviceslip.com/advice');
+		const data = await res.json();
+		adviceId.innerHTML = `ADVICE #${data.slip.id}`;
+		adviceDisplay.innerHTML = `"${data.slip.advice}"`;
+	} catch (e) {
+		adviceDisplay.innerHTML = `Error: ${e}`;
+		console.log(e);
+	}
+};
+
+btn.addEventListener('click', fetchWisdom);
